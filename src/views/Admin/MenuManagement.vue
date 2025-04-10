77fb12da1efdbfe9f539cc92fee1434f606464a4
DisonLv <438647864@qq.com>
2025-04-10 19:52:48 +0800

    增加子菜单页面

---
+++ src/views/Admin/MenuManagement.vue
@@ -0,0 +1,18 @@
+<!--
+  功能：菜单的维护
+  作者：disonlv
+  邮箱：disonlv@outlook.com
+  时间：2025年04月10日 19:40:42
+  版本：v1.0
+  修改记录：
+  修改内容：
+  修改人员：
+  修改时间：
+-->
+<template>
+  <n-card class="menu-management">这是子菜单维护部分</n-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
