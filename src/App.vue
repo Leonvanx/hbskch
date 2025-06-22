f493bacf660ba67c5bd0535fcd82131c983f1d3a
Lvin Xu <lvin_xu@outlook.com>
2025-06-22 16:00:47 +0800

    feat: 动态加载网站图标

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
 import { zhCN, dateZhCN } from 'naive-ui';
+import { searchResource } from './apis';
+const useDynamicIcon = () => {
+  let iconUrl = '';
+  const fetchIconUrl = async () => {
+    const res = await searchResource();
+    if (res.data) {
+      iconUrl = res.data.filter((it) => it.code === 'logo')[0].url;
+      updateFavicon(iconUrl);
+    }
+  };
+  const updateFavicon = (url: string) => {
+    if (!url) return;
+    const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
+    favicon.href = url;
+  };
+  onMounted(fetchIconUrl);
+};
+useDynamicIcon();
 </script>
 
 <template>
